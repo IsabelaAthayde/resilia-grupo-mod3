b44aeb85ed180d13947ff6ecd80632e7140821dc
Isabela Athayde <100873483+IsabelaAthayde@users.noreply.github.com>
2023-12-09 00:21:54 -0300

    Update maximun value in tables
    Os limites máximos de caracteres em colunas como: cpf e telefone, foram aumentados para que não ocorra ´TRUNCATE ERROR´

--- Database (Projeto).sql
+++ Database (Projeto).sql
@@ -4,9 +4,9 @@ USE escola;
 CREATE TABLE aluno( 
 id_aluno INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nome VARCHAR(60) NOT NULL,
-cpf VARCHAR(11) NOT NULL,
+cpf VARCHAR(25) NOT NULL,
 email VARCHAR(50) NOT NULL,
-telefone VARCHAR(11) NOT NULL,
+telefone VARCHAR(25) NOT NULL,
 endereco_completo VARCHAR(100) NOT NULL,
 status VARCHAR(20) NOT NULL
 );
@@ -19,12 +19,12 @@ turno VARCHAR (20) NOT NULL
 
 CREATE TABLE curso(
 id_curso INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-nome_curso VARCHAR(40) NOT NULL,
+nome_curso VARCHAR(200) NOT NULL,
 data_inicio DATE NOT NULL,
 data_termino DATE NOT NULL
 );
 
-CREATE TABLE modulos(
+CREATE TABLE modulo(
 id_modulo INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 data_inicio DATE NOT NULL,
 data_termino DATE NOT NULL
@@ -32,17 +32,16 @@ data_termino DATE NOT NULL
 
 CREATE TABLE disciplina(
 id_disciplina INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-nome_disciplina VARCHAR(50) NOT NULL
+nome_disciplina VARCHAR(70) NOT NULL
 );
 
 CREATE TABLE facilitador(
 id_facilitador INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nome VARCHAR(60) NOT NULL,
-cpf VARCHAR(11) NOT NULL,
+cpf VARCHAR(25) NOT NULL,
 email VARCHAR(50) NOT NULL,
-telefone VARCHAR(11) NOT NULL,
-endereco_completo VARCHAR(100) NOT NULL,
-disciplina VARCHAR(50) NOT NULL
+telefone VARCHAR(25) NOT NULL,
+endereco_completo VARCHAR(255) NOT NULL
 );
 
 ALTER TABLE aluno
@@ -55,12 +54,12 @@ ADD CONSTRAINT fk_id_curso_turma FOREIGN KEY (id_curso) REFERENCES curso(id_curs
 
 ALTER TABLE disciplina
 ADD COLUMN id_modulo INT(11) NOT NULL,
-ADD CONSTRAINT fk_id_modulo FOREIGN KEY (id_modulo) REFERENCES modulos(id_modulo);
+ADD CONSTRAINT fk_id_modulo FOREIGN KEY (id_modulo) REFERENCES modulo(id_modulo);
 
 ALTER TABLE disciplina
 ADD COLUMN id_facilitador INT(11) NOT NULL,
 ADD CONSTRAINT fk_id_facilitador FOREIGN KEY (id_facilitador) REFERENCES facilitador(id_facilitador);
 
-ALTER TABLE modulos
+ALTER TABLE modulo
 ADD COLUMN id_curso INT(11) NOT NULL,
-ADD CONSTRAINT fk_id_curso_modulos FOREIGN KEY (id_curso) REFERENCES curso(id_curso);
+ADD CONSTRAINT fk_id_curso_modulo FOREIGN KEY (id_curso) REFERENCES curso(id_curso);
